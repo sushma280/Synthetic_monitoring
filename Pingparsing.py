33b48cf9cb4b0d3ab2cdf8a909317b9a4b51dbc8
sushma.ayenampudi@gmail.com <your-email@example.com>
2025-04-03 09:15:48 -0500

    second commit

---
+++ Pingparsing.py
@@ -0,0 +1,12 @@
+import json
+import pingparsing
+
+ping_parser = pingparsing.PingParsing()
+transmitter = pingparsing.PingTransmitter()
+transmitter.destination = input("Enter the server name: ")
+transmitter.count = 20
+result = transmitter.ping()
+
+
+print("\n Ping Metrics \n")
+print(json.dumps(ping_parser.parse(result).as_dict(), indent=4))
